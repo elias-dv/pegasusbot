75066266aa6527597ba949dd9acc1d5db3644838
elias222228 <eliasdoidao.com.br@gmail.com>
2021-06-16 14:27:42 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5562995223384 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO PEGASUS  :*
+_https://chat.whatsapp.com/D2aCl8XrI8J5KG6jPZVhy6
+}
+exports.daftarvip = daftarvip
